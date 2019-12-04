624eb7db96bc7b2e91dbe33fddf1a079e9258a5b
DESKTOP-0GC2HJG\Антон <aist0025@gmail.com>
2019-12-04 21:24:33 +0500

    новый роутинг

--- models.py
+++ models.py
@@ -9,3 +9,10 @@ class Men(db.Model):
 
     def json(self):
         return {"id": self.id, "name": self.name}
+
+
+class Request(db.Model):
+    itemId = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer)
+    regNum = db.Column(db.String(120))
+    regDate = db.Column(db.Date)
