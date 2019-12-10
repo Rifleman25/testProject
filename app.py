d90c5ea900cc1dbd7b9c7ae2f23308b450a4c4bf
DESKTOP-0GC2HJG\Антон <aist0025@gmail.com>
2019-12-10 19:19:26 +0500

    Добвалены статусы

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-from models import db, Men, Person, Request, RequestType
+from models import db, Men, Person, Request, RequestType, RequestStatus
 from routes import api, index
 
 app = Flask(__name__)
@@ -9,17 +9,46 @@ app.register_blueprint(api)
 db.init_app(app)
 with app.app_context():
     db.create_all()
-    person = Person(name='Anton', surname='Istomin')
+
+    #Типы заявок
     provision_tsr = RequestType(name="Обеспечение ТСР")
     compensation_tsr = RequestType(name="Компенсация ТСР")
     rehab = RequestType(name="Ранняя реабилитация")
     db.session.add(provision_tsr)
     db.session.add(compensation_tsr)
     db.session.add(rehab)
-    db.session.add(person)
+
+    #Статусы завок
+    draft = RequestStatus(name="Черновик")
+    consideration = RequestStatus(name="Рассмотрение")
+    suspended = RequestStatus(name="Приостановлена")
+    closed = RequestStatus(name="Звкрыта")
+    completed = RequestStatus(name="Обеспечение закончено")
+    db.session.add(draft)
+    db.session.add(consideration)
+    db.session.add(suspended)
+    db.session.add(closed)
+    db.session.add(completed)
+
+    #Получатели услуг
+    anton = Person(name='Anton', surname='Istomin')
+    stas = Person(name='Станислав', surname='Тифакин')
+    sergey = Person(name='Сергей', surname='Иванов')
+
+    db.session.add(anton)
+    db.session.add(stas)
+    db.session.add(sergey)
     db.session.commit()
-    request = Request(reg_num='1', person_item_id=person.item_id, request_type_item_id=compensation_tsr.item_id)
-    db.session.add(request)
+
+    #Заявки
+    antonCompensation = Request(reg_num='1', person_item_id=anton.item_id, request_type_item_id=compensation_tsr.item_id, request_status_item_id=draft.item_id)
+    antonRehab = Request(reg_num='35', person_item_id=anton.item_id, request_type_item_id=rehab.item_id, request_status_item_id=closed.item_id)
+    stasprovision = Request(reg_num='1514', person_item_id=stas.item_id, request_type_item_id=provision_tsr.item_id, request_status_item_id=consideration.item_id)
+    antonCompensation = Request(reg_num='1', person_item_id=sergey.item_id, request_type_item_id=compensation_tsr.item_id, request_status_item_id=completed.item_id)
+    db.session.add(antonCompensation)
+    db.session.add(antonRehab)
+    db.session.add(stasprovision)
+    db.session.add(antonCompensation)
     db.session.commit()
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -20,10 +20,12 @@ class Request(db.Model):
     create_date = db.Column(db.DateTime(timezone=True), server_default=func.now())
     update_date = db.Column(db.DateTime(timezone=True), onupdate=func.now())
     request_type_item_id = db.Column(db.Integer, ForeignKey('requestType.item_id'))
+    request_status_item_id = db.Column(db.Integer, ForeignKey('requestStatus.item_id'))
     person_item_id = db.Column(db.Integer, ForeignKey('person.item_id'))
 
     person = relationship('Person')
     requestType = relationship('RequestType')
+    requestStatus = relationship('RequestStatus')
 
     def json(self):
         return {"item_id": self.item_id, "reg_num": self.reg_num, "person": self.person.json()}
@@ -43,6 +45,16 @@ class Person(db.Model):
 
 class RequestType(db.Model):
     __tablename__ = 'requestType'
+    item_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+
+    def json(self):
+        return {"item_id": self.item_id, "name": self.name}
+
+
+
+class RequestStatus(db.Model):
+    __tablename__ = 'requestStatus'
     item_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120))
 
